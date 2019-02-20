fce289cf470e84ecceda4d95d3cca9d09079d60f
Adam Penczek <adam.penczek11@gmail.com>
2019-02-20 15:55:47 +0100

    Manipulating resources added.

--- AlbumInfo.API/Controllers/TracksController.cs
+++ AlbumInfo.API/Controllers/TracksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlbumInfo.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,10 +22,11 @@ namespace AlbumInfo.API.Controllers
             return Ok(album.Tracks);
        }
 
-        [HttpGet("{albumId}/tracks/{trackId}")]
+        [HttpGet("{albumId}/tracks/{trackId}", Name = "GetTrack")]
         public IActionResult GetTrack(int albumId, int trackId)
         {
             var album = AlbumDataStore.Current.Albums.FirstOrDefault(a => a.Id == albumId);
+
             if (album == null)
                 return NotFound();
 
@@ -32,6 +35,82 @@ namespace AlbumInfo.API.Controllers
                 return NotFound();
 
             return Ok(track);
+        }
+
+        [HttpPost("{albumId}/tracks")]
+        public IActionResult CreateTrack(int albumId,
+            [FromBody] TrackForCreationDto track)
+        {
+            if (track == null)
+                return BadRequest();
+
+            var album = AlbumDataStore.Current.Albums.FirstOrDefault(a => a.Id == albumId);
+
+            if (album == null)
+                return NotFound();
+
+            var lastTrackId = AlbumDataStore.Current.Albums.SelectMany(a => a.Tracks).Max(p => p.Id);
+
+            var finalTrack = new TrackDto()
+            {
+                Id = ++lastTrackId,
+                Name = track.Name,
+                Duration = track.Duration
+            };
+
+            album.Tracks.Add(finalTrack);
+
+            return CreatedAtRoute("GetTrack", new
+            { albumId = albumId, trackId = finalTrack.Id }, finalTrack);
+
+        }
+
+        [HttpPut("{albumId}/tracks/{trackId}")]
+        public IActionResult UpdateTrack(int albumId, int trackId,
+            [FromBody] TrackForCreationDto track)
+        {
+            if (track == null)
+                return BadRequest();
+
+            var album = AlbumDataStore.Current.Albums.FirstOrDefault(a => a.Id == albumId);
+
+            if (album == null)
+                return NotFound();
+
+            var trackToUpdate = album.Tracks.FirstOrDefault(t => t.Id == trackId);
+
+            if (trackToUpdate == null)
+                return NotFound();
+
+            trackToUpdate.Name = track.Name;
+            trackToUpdate.Duration = track.Duration;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{albumId}/tracks/{trackId")]
+        public IActionResult PartialUpdateTrack(int albumId, int trackId,
+            [FromBody] JsonPatchDocument<TrackForCreationDto> patchDoc)
+        {
+            if (patchDoc == null)
+                return BadRequest();
+
+            var album = AlbumDataStore.Current.Albums.FirstOrDefault(a => a.Id == albumId);
+
+            if (album == null)
+                return NotFound();
+
+            var trackToUpdate = album.Tracks.FirstOrDefault(t => t.Id == trackId);
+
+            if (trackToUpdate == null)
+                return NotFound();
+
+            var trackToPatch =
+                new TrackForCreationDto()
+                {
+                    Name = trackToUpdate.Name,
+                    Duration = trackToUpdate.Duration
+                };
         }
     }
 }
---
+++ AlbumInfo.API/Models/TrackForCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumInfo.API.Models
+{
+    public class TrackForCreationDto
+    {
+        public string Name { get; set; }
+        public float Duration { get; set; }
+    }
+}
